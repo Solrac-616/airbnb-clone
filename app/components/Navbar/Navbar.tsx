c2128b6f1e2f2a80a8d375e7847e3a6dd905062c
Carlos Brito <carlos.e1998g@gmail.com>
2023-06-09 17:10:30 -0400

    test navbar

---
+++ app/components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import Container from "../Container";
+
+const Navbar = () => {
+    return ( 
+        <div className="fixed w-full bg-white z-40 shadow-sm">
+            <div className="py-4 border-b-[1px]">
+                this is the Navbar
+            </div>
+            <Container>
+                
+            </Container>
+        </div>
+     );
+}
+ 
+export default Navbar;
