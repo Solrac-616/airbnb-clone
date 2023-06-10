e712a2564e85204fe81e500b8d2b30dca2df7dec
Carlos Brito <carlos.e1998g@gmail.com>
2023-06-10 16:52:43 -0400

    final menu

--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -2,6 +2,7 @@
 import { AiOutlineMenu} from 'react-icons/ai'
 import Avatar from '../Avatar';
 import { useState, useCallback } from 'react';
+import MenuItem from './MenuItem';
 
 const UserMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -22,7 +23,7 @@ const UserMenu = () => {
           Home
         </div>
         <div className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shdow-md transition"
-          onClick={() => {}}
+          onClick={toggleOpen}
         >
           <AiOutlineMenu />
           <div className='hidden md:block'>
@@ -30,6 +31,23 @@ const UserMenu = () => {
           </div>
         </div>
       </div>
+
+      {isOpen && (
+        <div className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm'>
+          <div className='flex flex-col cursor-pointer'>
+            <>
+              <MenuItem 
+                onclick={() => {}}
+                label='Login'
+              />
+              <MenuItem 
+                onclick={() => {}}
+                label='Sign Up'
+              />
+            </>
+          </div>
+        </div>
+      )}
     </div>
    );
 }
